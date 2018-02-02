5daade24a1a63b1a4737a07e10853723929d3a94
BackStreetSoy <jordanedwardobey@gmail.com>
2018-02-02 00:43:23 -0500

    added comments

--- basics.rb
+++ basics.rb
@@ -1,15 +1,19 @@
+#requiring gems 
 require 'rubygems'
 require 'sinatra'
 
+#landing page, just says hello world
 get '/' do 
     "Hello, World!"
 end 
+
 
 get '/about' do 
     "Hey there, this is about me. "
 
 end 
 
+#:name is a key in the params array, which holds all GET and POST variables
 get '/hello/:name' do 
     "hello there, #{params[:name].reverse.upcase}"
 end 
@@ -18,13 +22,17 @@ get '/more/*' do
     "THANK YOU FOR YOUR #{params[:splat].last.upcase}"
 end 
 
+#this references a view file named form.erb
 get '/form' do 
     erb :form 
 end 
 
+#returns the 'message' input of the submit form from the form.erb view 
 post '/form' do
   "You said '#{params[:message]}'"
 end
+
+#the rest of this exercise is practice manipulating form inputs
 
 get '/secret' do 
     erb :secret
